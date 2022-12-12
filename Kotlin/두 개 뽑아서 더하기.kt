d62dd7a2ce79cb1cca5f9bcb704d09f15f273906
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2022-12-13 01:04:13 +0900

    update 두 개 뽑아서 더하기
    다른 사람 풀이 추가

--- Kotlin/두 개 뽑아서 더하기.kt
+++ Kotlin/두 개 뽑아서 더하기.kt
@@ -17,3 +17,16 @@ class Solution {
         return answer
     }
 }
+//다른 사람 풀이
+//class Solution {
+//    fun solution(numbers: IntArray): IntArray {
+//        var answer = mutableSetOf<Int>()
+//
+//        for((index, value) in numbers.withIndex()) {
+//            for(i in index + 1 until numbers.size) {
+//                answer.add(value + numbers[i])
+//            }
+//        }
+//        return answer.sorted().toIntArray()
+//    }
+//}
