888795957a59370a9a18f3365b1018b4f4547c8a
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2022-12-13 16:06:57 +0900

    Update(실패율): 다른 사람 풀이 추가

--- Kotlin/실패율.kt
+++ Kotlin/실패율.kt
@@ -40,4 +40,36 @@ class Solution {
         return answer
     }
 }
-//[3,4,2,1,5]
+//다른 사람 풀이
+//class Solution {
+//    fun solution(N: Int, stages: IntArray): IntArray {
+//        var answer = IntArray(N)
+//
+//        val indexMap: MutableMap<Int, Int> = mutableMapOf()
+//        val failureMap: MutableMap<Int, Double> = mutableMapOf()
+//        stages.forEach {
+//            when (indexMap.containsKey(it)) {
+//                true -> indexMap[it] = indexMap.getValue(it) + 1
+//                false -> indexMap[it] = 1
+//            }
+//        }
+//        var totalSize = stages.size
+//
+//        for (i in 1..N) {
+//            when (indexMap.containsKey(i)) {
+//                true -> {
+//                    failureMap[i] = (indexMap.getValue(i) / totalSize.toDouble())
+//                    totalSize -= indexMap.getValue(i)
+//                }
+//                false -> failureMap[i] = 0.0
+//            }
+//        }
+//        val list = failureMap.toList().sortedByDescending { (index, value) -> value }
+//
+//        for (i in 0 until N) {
+//            answer[i] = list[i].first
+//        }
+//
+//        return answer
+//    }
+//}
