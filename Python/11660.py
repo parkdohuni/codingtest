217bd065214aad9cbe3d5a0f98408137868b72ac
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2022-12-23 21:23:02 +0900

    11660번 수정

--- Python/11660.py
+++ Python/11660.py
@@ -13,5 +13,5 @@ for i in range(1, n+1):
 
 for _ in range(m):
     x1, y1, x2, y2 = map(int, input().split())
-    result = sum_arr[x2+1][y2+1] - sum_arr[x1][y2+1] - sum_arr[x2+1][y1] + sum_arr[x1][y1]
+    result = sum_arr[x2][y2] - sum_arr[x1 - 1][y2] - sum_arr[x2][y1 - 1] + sum_arr[x1 - 1][y1 - 1]
     print(result)
