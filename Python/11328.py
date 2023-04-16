9667f39facc1fe7bea9713b5060f9a4168cb3300
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-04-16 17:44:37 +0900

    Create 11328.py

---
+++ Python/11328.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+for i in range(n):
+    a, b = input().split()
+    a = ''.join(sorted(a))
+    b = ''.join(sorted(b))
+    if len(a) != len(b):
+        print("Impossible")
+        continue
+    if a == b:
+        print("Possible")
+    else:
+        print("Impossible")
+    
