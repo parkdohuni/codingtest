2f9c9a5c2a761d2a2a6018d0ede49d33c8119a53
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-04-16 23:21:23 +0900

    Create 1406.py

---
+++ Python/1406.py
@@ -0,0 +1,26 @@
+#1406번 에디터
+import sys
+
+st1 = list(sys.stdin.readline().rstrip())
+st2 = []
+n = int(sys.stdin.readline())
+for _ in range(n):
+    cmd = list(sys.stdin.readline().split())
+    if cmd[0] == 'L':
+        if st1:
+            st2.append(st1.pop())
+    
+    elif cmd[0] == 'D':
+        if st2:
+            st1.append(st2.pop())
+        
+    elif cmd[0] == 'B':
+        if st1:
+            st1.pop()
+            
+    else:
+        st1.append(cmd[1])
+
+st1.extend(reversed(st2))
+print(''.join(st1))
+print(st1)
