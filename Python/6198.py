81799c8f292686fb6457ec55773c4a3714f893ac
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-04-27 17:40:03 +0900

    Update 6198.py

--- Python/6198.py
+++ Python/6198.py
@@ -6,5 +6,15 @@ arr = []
 
 for _ in range(n):
     arr.append(int(sys.stdin.readline()))
+    
+stack = []
+ans = 0
 
-print(arr)
+for i in arr:
+    while stack and stack[-1] <= i:
+        stack.pop()
+    stack.append(i)
+    
+    ans += len(stack) - 1
+    
+print(ans)
