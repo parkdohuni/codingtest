10f299db980b7340ecbb9d7c006ed913b99734e7
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-05-04 01:26:08 +0900

    Update 5430.py

--- Python/5430.py
+++ Python/5430.py
@@ -1,23 +1,32 @@
 import sys
 from collections import deque
 t = int(sys.stdin.readline())
-flag = 0
 for _ in range(t):
     p = sys.stdin.readline().rstrip()
     n = int(sys.stdin.readline())
     x = sys.stdin.readline().rstrip()[1:-1].split(",")
     queue = deque(x)
+    rev = 0
+    flag = 0
     
     for i in p:
-        if i == "R":
-            queue.reverse()
-        elif i == "D":
-            if queue:
-                queue.popleft()
-            else:
+        if i == 'R':
+            rev += 1
+        elif i == 'D':
+            if len(queue) < 1:
                 flag = 1
-                
-    if flag == 1:
-        print("error")
-    else:
-        print(queue)
+                print("error")
+                break
+            else:
+                if rev % 2 == 0:
+                    queue.popleft()
+                else:
+                    queue.pop()
+                    
+    if flag == 0:
+        if rev % 2 == 0:
+            print("["+",".join(queue) + "]")
+        else:
+            queue.reverse()
+            print("["+",".join(queue) + "]")
+        
