3f6a0111bfd852af08ed3ba51d00418941f2564f
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-05-06 23:36:17 +0900

    Update 2178.py

--- Python/2178.py
+++ Python/2178.py
@@ -1,7 +1,6 @@
 import sys
 from collections import deque
 def bfs(x, y):
-    ans = 1
     dx = [1, -1, 0, 0]
     dy = [0, 0, 1, -1]
     q = deque()
@@ -14,7 +13,6 @@ def bfs(x, y):
             if 0 <= nx < n and 0 <= ny < m and arr[nx][ny] == 1:
                 q.append([nx, ny])
                 arr[nx][ny] = arr[x][y] + 1
-                ans += 1
                 
     return arr[n-1][m-1]
                 
