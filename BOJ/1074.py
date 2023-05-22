f453554c7aa21c203bf18fc3e3d0d90c0221ec52
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-05-22 21:18:16 +0900

    Update 1074.py

--- BOJ/1074.py
+++ BOJ/1074.py
@@ -1,19 +1,11 @@
 n, r, c = map(int, input().split())
 
 
-# n이 1일 때 탐색한다?
-
-def zz(n, r, c):
+def sol(n, r, c):
     if n == 0:
         return 0
-    else:
-        half = 2 ** (n - 1)
-        if r < half and c < half:
-            return zz(n - 1, r, c)
-        if r < half <= c:
-            return half * half + zz(n - 1, r, c - half)
-        if r > half > c:
-            return 2 * half * half + zz(n - 1, r - half, c)
+
+    return 2 * (r % 2) + (c % 2) + 4 * sol(n - 1, int(r / 2), int(c / 2))
 
 
-print(zz(n, c, r))
+print(sol(n, r, c))
