428be3242b39829aea3758bbf452c9afc9baae2a
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-05-26 17:40:14 +0900

    Update 4.py

--- programmers/4.py
+++ programmers/4.py
@@ -1,33 +1,39 @@
 def solution(plans):
     answer = []
-    stop = []
-    plans.sort(key=lambda x: (x[1]))
+    plans.sort(key=lambda x: x[1])
     print(plans)
-    time = []
-    ehour = 0
-    emin = 0
-    for i in range(len(plans)):
-        if len(s) == 0:
-            s = plans[i][1].split(":")
-        shour, smin = list(map(int, s))
+    stack = []
+    for subject, start, time in plans:
+        h, m = map(int, start.split(':'))
+        start = 60 * h + m
+        time = int(time)
 
-        if ehour > shour:
-            stop.append([list(plans[i][0]), ehour, emin])
-        elif ehour == shour:
-            if emin > smin:
-                stop.append([list(plans[i][0]), ehour, emin])
-        
-        ehour = shour
-        emin = smin
-        emin += list(map(int, plans[i][2]))
-        while emin >= 60:
-            emin -= 60
-            ehour += 1
+        if stack:
+            prev_subject, prev_start, prev_time = stack.pop()
+            extra = start - prev_start
 
+            if extra < prev_time:
+                stack.append((prev_subject, prev_start, prev_time - extra))
+            else:
+                answer.append(prev_subject)
+                extra = extra - prev_time
+
+                while stack and extra:
+                    prev_subject, prev_start, prev_time = stack.pop()
+                    if extra < prev_time:
+                        stack.append((prev_subject, prev_start, prev_time - extra))
+                        break
+                    else:
+                        answer.append(prev_subject)
+                        extra = extra - prev_time
+
+        stack.append((subject, start, time))
+
+    answer.extend([s for s, _, _ in stack[::-1]])
     return answer
 
 
-solution([["korean", "11:40", "30"], ["english", "12:10", "20"], ["math", "12:30", "40"]])
+# solution([["korean", "11:40", "30"], ["english", "12:10", "20"], ["math", "12:30", "40"]])
 solution(
     [["science", "12:40", "50"], ["music", "12:20", "40"], ["history", "14:00", "30"], ["computer", "12:30", "100"]])
 solution([["aaa", "12:00", "20"], ["bbb", "12:10", "30"], ["ccc", "12:40", "10"]])
