e2c9adcfc74ef610390b3dcf13ed80f9dff0ee32
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-06-21 12:58:47 +0900

    Create 22.py

---
+++ programmers/22.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = []
+    a = s.split(" ")
+    for i in a:
+        if i:
+            answer.append(i[0].upper() + i[1:].lower())
+        else:
+            answer.append(i)
+
+    return " ".join(answer)
