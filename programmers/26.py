51b71944b79336451458cf9e2efce200afb17cef
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-06-23 01:43:51 +0900

    Create 26.py

---
+++ programmers/26.py
@@ -0,0 +1,13 @@
+def solution(cacheSize, cities):
+    answer = 0
+    return answer
+
+
+solution(3, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"])
+solution(3, ["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"])
+solution(2, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork",
+             "Rome"])
+solution(5, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork",
+             "Rome"])
+solution(2, ["Jeju", "Pangyo", "NewYork", "newyork"])
+solution(0, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA"])
