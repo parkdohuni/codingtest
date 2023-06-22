a2ee08d3cff4626f2b4a1f4965ff804f70624eef
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-06-22 16:56:34 +0900

    Create 25.py

---
+++ programmers/25.py
@@ -0,0 +1,15 @@
+def solution(n):
+    ans = 1
+
+    while n != 1:
+        if n % 2 == 0:
+            n = n / 2
+        else:
+            n -= 1
+            ans += 1
+
+    return ans
+
+
+def solution(n):
+    return bin(n).count('1')
