21d3d1789e24485990fe771ac602c8643abb0eb6
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-07-02 17:11:53 +0900

    Update 30.py

--- programmers/30.py
+++ programmers/30.py
@@ -1,16 +1,11 @@
 n = int(input())
 
-dp = [[0 for i in range(n)] for _ in range(3)]
+dp = [list(map(int, input().split())) for _ in range(n)]
 
-R = [0] * 3
-G = [0] * 3
-B = [0] * 3
-dp[1][0] = R[0]
-dp[1][1] = G[0]
-dp[1][2] = B[0]
-for i in range(2, n + 1):
-    dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + R[1]
-    dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + G[1]
-    dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + B[1]
 
-print(min(dp[n][0], dp[n][1], dp[n][2]))
+for i in range(1, len(dp)):
+    dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + dp[i][0]
+    dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + dp[i][1]
+    dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + dp[i][2]
+
+print(min(dp[n-1][0], dp[n-1][1], dp[n-1][2]))
