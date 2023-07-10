145ed65c29abaa1edf30ad3546714841b8f8611c
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-07-11 00:54:38 +0900

    Update 11055.py

--- BOJ/11055.py
+++ BOJ/11055.py
@@ -1,4 +1,12 @@
-n = int(input())
+import sys
 
-arr = list(map(int, int(input().split())))
+n = int(sys.stdin.readline().strip())
+a = [int(x) for x in sys.stdin.readline().split()]
+dp = a[:]
 
+for i in range(n):
+    for j in range(i):
+        if a[i] > a[j]:
+            dp[i] = max(dp[i], dp[j] + a[i])
+
+print(max(dp))
