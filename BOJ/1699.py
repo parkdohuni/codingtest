0af7b5298a1ff82f5e2577e3cb8148fb863a230d
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-07-18 18:50:11 +0900

    Create 1699.py

---
+++ BOJ/1699.py
@@ -0,0 +1,13 @@
+import math
+
+n = int(input())
+
+d = [k for k in range(n+1)]
+
+for i in range(1, n + 1):
+    for j in range(1, i):
+        if j * j > i:
+            break
+        if d[i] > d[i - j * j] + 1:
+            d[i] = d[i - j * j] + 1
+print(d[n])
