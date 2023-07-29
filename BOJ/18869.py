0c0d6f3b463dd54b2fb6b8f34d0887857e5ec223
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-07-29 19:03:20 +0900

    Update 18869.py

--- BOJ/18869.py
+++ BOJ/18869.py
@@ -8,10 +8,15 @@ universe = defaultdict(int)
 
 for _ in range(m):
     planets = list(map(int, input().split()))
+    # 12 50 31
     keys = sorted(list(set(planets)))
+    # 12 31 50
     ranks = {keys[i]: i for i in range(len(keys))}
+    # 12:0 31:1 50:2
     add = tuple([ranks[x] for x in planets])
+    # 0 2 1
     universe[add] += 1
+    # (0 2 1): 1
 
 ans = 0
 
