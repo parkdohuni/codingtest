f08bd1b345ede8d0a889eb16bbd252173d5ab28a
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-08-10 15:29:44 +0900

    Update 1915.py

--- BOJ/1915.py
+++ BOJ/1915.py
@@ -1,18 +1,22 @@
-n, m = list(map(int, input().split()))
+import sys
 
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
 arr = []
+for _ in range(n):
+    arr.append(list(map(int, input().rstrip())))
+
 dp = [[0] * m for _ in range(n)]
-
-for i in range(n):
-    arr.append(list(map(int, input())))
-
 ans = 0
+
 for i in range(n):
     for j in range(m):
         if i == 0 or j == 0:
             dp[i][j] = arr[i][j]
         elif arr[i][j] == 1:
             dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1
-        ans = max(dp[i][j], ans)
+
+        ans = max(ans, dp[i][j])
 
 print(ans * ans)
