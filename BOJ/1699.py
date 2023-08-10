299cae071ab37d57403ff984afc77124a38e3cbc
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-08-10 13:43:25 +0900

    Update 1699.py

--- BOJ/1699.py
+++ BOJ/1699.py
@@ -1,13 +1,16 @@
-import math
+import sys
+
+input = sys.stdin.readline
 
 n = int(input())
 
-d = [k for k in range(n+1)]
+dp = [k for k in range(n+1)]
 
-for i in range(1, n + 1):
+for i in range(1, n+1):
     for j in range(1, i):
         if j * j > i:
             break
-        if d[i] > d[i - j * j] + 1:
-            d[i] = d[i - j * j] + 1
-print(d[n])
+        if dp[i] > dp[i - j * j] + 1:
+            dp[i] = dp[i - j * j] + 1
+
+print(dp[n])
