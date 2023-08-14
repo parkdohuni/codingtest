ff0a4624da40f73ef9890d4b77cded4883e2549b
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-08-15 00:37:51 +0900

    Update 5567.py

--- BOJ/5567.py
+++ BOJ/5567.py
@@ -1,5 +1,5 @@
 import sys
-
+from collections import deque
 input = sys.stdin.readline
 
 n = int(input())
@@ -12,3 +12,20 @@ for _ in range(m):
     graph[u].append(v)
     graph[v].append(u)
 
+visited = [False] * (n + 1)
+def bfs(graph, start, visited):
+
+    queue = deque([start])
+    visited[start] = True
+    friends = [1] + graph[1]
+    ans = []
+    while queue:
+        a = queue.popleft()
+        for i in graph[a]:
+            if not visited[i] and a in friends:
+                queue.append(i)
+                visited[i] = True
+                ans.append(i)
+    print(len(ans))
+
+bfs(graph, 1, visited)
