96fb54bf668c10be52d32ea56379a433dc55a279
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-08-14 23:48:38 +0900

    Create 5567.py

---
+++ BOJ/5567.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
