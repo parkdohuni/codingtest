ac5a728898e28153c5c91d7f97acf65fbd5bb467
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-09-07 19:59:11 +0900

    Create 5214.py

---
+++ BOJ/5214.py
@@ -0,0 +1,37 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+
+def bfs():
+    q = deque()
+    q.append((1, nvisited, 1))
+    while q:
+        subway, visited, level = q.popleft()
+        if subway == n:
+            print(level)
+            sys.exit()
+
+        for s in graph[subway]:
+            if visited[s] == False:
+                visited[s] = True
+                if s > n:
+                    q.append((s, visited, level))
+                else:
+                    q.append((s, visited, level + 1))
+
+
+n, k, m = map(int, input().split())
+
+graph = [[] for _ in range(n + m + 1)]
+
+for i in range(1, m + 1):
+    sub = list(map(int, input().split()))
+    graph[n + i] = sub
+    for s in sub:
+        graph[s].append(n + i)
+
+nvisited = [False] * (n + m + 1)
+bfs()
+print(-1)
