5ac0bd770ffefcab25f650c248553af6a3e90f27
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-10-01 00:09:40 +0900

    Create 15655.py

---
+++ BOJ/15655.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+ans = []
+visited = [False] * (n + 1)
+arr = sorted(arr)
+def back(idx):
+    if len(ans) == m:
+        print(*ans)
+        return
+    for i in range(idx, n + 1):
+        if visited[i]:
+            continue
+        visited[i] = True
+        ans.append(arr[i - 1])
+        back(i)
+        ans.pop()
+        visited[i] = False
+
+back(1)
