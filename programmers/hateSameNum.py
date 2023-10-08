4157a582a26956556d08ecd5ad24572f27e82075
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-10-08 19:00:08 +0900

    Create hateSameNum.py

---
+++ programmers/hateSameNum.py
@@ -0,0 +1,11 @@
+def solution(arr):
+    answer = []
+    answer.append(arr[0])
+    for i in arr:
+        if answer[-1] != i:
+            answer.append(i)
+    return answer
+
+
+print(solution([1, 1, 3, 3, 0, 1, 1]))
+print(solution([4, 4, 4, 3, 3]))
