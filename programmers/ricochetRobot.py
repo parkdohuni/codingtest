29039fe7386050b48405d04921f89211232aa5a5
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-10-23 13:30:32 +0900

    Update ricochetRobot.py

--- programmers/ricochetRobot.py
+++ programmers/ricochetRobot.py
@@ -1,17 +1,42 @@
-def solution(board):
-    answer = 0
-    x = 0
-    y = 0
-    for i in range(len(board)):
-        for j in range(len(board[i])):
-            if board[i][j] == "R":
-                x = i
-                y = j
-                break
+from collections import deque
 
-    print(x)
-    print(y)
-    return answer
+
+def solution(board):
+    q = deque()
+    x_max = len(board[0])
+    y_max = len(board)
+    visited = [[-1 for _ in range(x_max)] for _ in range(y_max)]
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    def move(x, y, i):
+        while True:
+            x += dx[i]
+            y += dy[i]
+            if x < 0 or y < 0 or x >= x_max or y >= y_max:
+                break
+            if board[y][x] == 'D':
+                break
+        return y - dy[i], x - dx[i]
+
+    for i in range(y_max):
+        for j in range(x_max):
+            if board[i][j] == "R":
+                q.append([i, j])
+                visited[i][j] = 0
+            if board[i][j] == "G":
+                x_goal = j
+                y_goal = i
+
+    while q:
+        y, x = q.popleft()
+        for i in range(4):
+            ny, nx = move(x, y, i)
+            if visited[ny][nx] == -1:
+                q.append([ny, nx])
+                visited[ny][nx] = visited[y][x] + 1
+
+    return visited[y_goal][x_goal]
 
 
 print(solution(["...D..R", ".D.G...", "....D.D", "D....D.", "..D...."]))
