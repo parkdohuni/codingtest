6f0cb3b90f27a72900a6057bc2f0cca94c39366c
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-11-24 15:23:57 +0900

    Update 14699.py

--- BOJ/14699.py
+++ BOJ/14699.py
@@ -1,13 +1,37 @@
 import sys
+from collections import deque
+
+
+def bfs(start):
+    visited = [False] * (n + 1)
+    cnt = 1
+    q = deque([(start, cnt)])
+    while q:
+        cur, cnt = q.popleft()
+
+        if visited[cur]:
+            continue
+
+        visited[cur] = True
+
+        for i in graph[cur]:
+            if not visited[i] and height[i] > height[cur]:
+                cnt += 1
+                q.append((i, cnt))
+
+    return cnt
+
+
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-arr = list(map(int, input().split()))
-graph = [[0] * (m + 1) for _ in range(m + 1)]
+height = [0] + list(map(int, input().split()))
+graph = [[] for _ in range(n + 1)]
 for _ in range(m):
     u, v = map(int, input().split())
-    graph[u][v] = 1
-    graph[v][u] = 1
+    graph[u].append(v)
+    graph[v].append(u)
 
-for i in graph:
-    print(i)
+for i in range(1, n + 1):
+    result = bfs(i)
+    print(result)
