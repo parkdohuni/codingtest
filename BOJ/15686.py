9f772aff014c1d3768a432b72f75e111ab11d9ee
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-11-30 00:28:08 +0900

    Create 15686.py

---
+++ BOJ/15686.py
@@ -0,0 +1,35 @@
+def close(a,b): # 요구사항인 M개 만큼 치킨집을 폐업해보자
+    global distance_min
+    if a > len(chicken):
+        return
+    if b == M:
+        distance_all = 0
+        for r, c in house:
+            distance = float('inf')
+            for i in val:
+                rr, cc = chicken[i]
+                distance = min(distance, abs(rr-r)+abs(cc-c))
+            distance_all += distance
+        distance_min = min(distance_min, distance_all)
+        return
+
+    val.append(a)
+    close(a+1, b+1)
+    val.pop()
+    close(a+1,b)
+
+import sys
+N, M = map(int, sys.stdin.readline().split())
+city = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+chicken, house, val = [], [], []
+
+for i in range(N):
+    for j in range(N):
+        if city[i][j] == 1:
+            house.append([i,j])
+        elif city[i][j] == 2:
+            chicken.append([i,j])
+
+distance_min = float('inf')
+close(0,0)
+print(distance_min)
