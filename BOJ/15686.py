43595ac19b255af6dfc7564f3787d60a264adab4
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-11-30 16:25:33 +0900

    Update 15686.py

--- BOJ/15686.py
+++ BOJ/15686.py
@@ -1,35 +1,38 @@
-def close(a,b): # 요구사항인 M개 만큼 치킨집을 폐업해보자
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+city = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+chicken, house, val = [], [], []
+
+
+def close(a, b):  # 요구사항인 M개 만큼 치킨집을 폐업해보자
     global distance_min
     if a > len(chicken):
         return
     if b == M:
         distance_all = 0
-        for r, c in house:
+        for x, y in house:
             distance = float('inf')
             for i in val:
-                rr, cc = chicken[i]
-                distance = min(distance, abs(rr-r)+abs(cc-c))
-            distance_all += distance
+                r, c = chicken[i]
+                dist = min(distance, abs(r - x) + abs(c - y))
+            distance_all += dist
         distance_min = min(distance_min, distance_all)
         return
 
     val.append(a)
-    close(a+1, b+1)
+    close(a + 1, b + 1)
     val.pop()
-    close(a+1,b)
+    close(a + 1, b)
 
-import sys
-N, M = map(int, sys.stdin.readline().split())
-city = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-chicken, house, val = [], [], []
 
 for i in range(N):
     for j in range(N):
         if city[i][j] == 1:
-            house.append([i,j])
+            house.append([i, j])
         elif city[i][j] == 2:
-            chicken.append([i,j])
+            chicken.append([i, j])
 
 distance_min = float('inf')
-close(0,0)
+close(0, 0)
 print(distance_min)
